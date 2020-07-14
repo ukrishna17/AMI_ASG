504f9b4b074a876c7b55c50e41c5593f503c4fa9
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-14 11:53:07 +0530

    Update launchwebsite.sh

--- launchwebsite.sh
+++ launchwebsite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-systemctl stop apache2
-rm -rf /var/www/html/2117_infinite_loop/*
-wget https://www.tooplate.com/zip-templates/2115_marvel.zip
-unzip -o 2115_marvel.zip
-cp -r 2115_marvel/* /var/www/html/
-systemctl restart apache2
+sudo systemctl stop apache2
+sudo rm -rf /var/www/html/*
+sudo wget https://www.tooplate.com/zip-templates/2115_marvel.zip
+sudo unzip -o 2115_marvel.zip
+sudo cp -r 2115_marvel/* /var/www/html/
+sudo systemctl restart apache2
