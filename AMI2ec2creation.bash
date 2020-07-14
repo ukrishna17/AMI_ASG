c333b4f3f5cac2f2ef2ce231901cda0be32fb3a6
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-14 12:39:02 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -4,11 +4,13 @@ AMIimg_id="ami-065334bec91050778"
 keyname="Terra-Key"
 groupId="terra-sg"
 
-aws ec2 run-instances \
+instance_id=$(aws ec2 run-instances \
         --image-id $AMIimg_id \
         --instance-type t2.micro \
         --key-name $keyname \
         --user-data file://launchwebsite.sh \
         --security-group-ids $groupId \
         --query 'Instances[*].[InstanceId]' \
-        --output text
+        --output text)
+        
+ image_id=$(aws ec2 create-image --instance-id $instance_id --name "AMItest_V2" --description "AMI for my webserver version2" --query ImageId --output text)
