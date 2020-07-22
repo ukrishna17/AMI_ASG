300aae0fad434d5f1a3773db5d4019b81120ef7d
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-22 09:40:50 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -1,9 +1,10 @@
 version=$1
+instance_id=$2
+asgname=$3
 keyname="Terra-Key"
 groupId="terra-sg"
 lcKeyname="asg-keypair"
 lc_Sg="sg-093e2727028157daa"
-instance_id="i-030681284d45db03e"
 AMIVersion="AMITest_$version"
 LCVersion="LCTest_$version"
 #image_id="ami-0ef38fe45b30d30f6"
@@ -11,11 +12,11 @@ LCVersion="LCTest_$version"
 image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
 
 sleep 3m
-aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --instance-type t2.micro --security-groups $lc_Sg 
+aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
 
 sleep 2m
 aws autoscaling update-auto-scaling-group \
-				--auto-scaling-group-name "Autoscaling-testing" \
+				--auto-scaling-group-name $asgname \
 				--launch-configuration-name $LCVersion \
 				--min-size 1 \
 				--desired-capacity 1\
