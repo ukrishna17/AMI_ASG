f02193873480e461a1805e6c12c85d6a07a38aa8
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-22 11:30:05 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -8,10 +8,11 @@ LCVersion="LCTest_$version"
 #image_id="ami-0ef38fe45b30d30f6"
 
 
-if [ $version != "" ] && [ $instance_id != "" ] && [ $asgname != "" ] 
+if [ -z $version ] && [ -z $instance_id ] && [ -z $asgname ] 
 then
-    if [ "aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].[InstanceId] --output text" != "" ] 
+    if [ -z "aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].[InstanceId] --output text" ] 
     then 
+ 
         image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
 	sleep 80
 	aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
@@ -23,7 +24,7 @@ then
 				--desired-capacity 1\
 				--max-size 1
      else 
-	  echo "no instance existed with the given instance"
+	  echo "no instance existed with this instance id:$instance_id"
      fi
 else
     echo "Don't Pass empty strings"
