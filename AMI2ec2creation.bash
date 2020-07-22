f4b6b3e2811ebf2c747a6c1d078f09bfba659584
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-22 12:12:05 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -11,21 +11,21 @@ existed=$(aws ec2 describe-instances --instance-ids $instance_id --query Reserva
 
 if [ -z $version ] && [ -z $instance_id ] && [ -z $asgname ] 
 then
-    if [ -z $existed ] 
-    then 
-        image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
-    	sleep 80
-        aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
-        sleep 40
-        aws autoscaling update-auto-scaling-group \
+   if [ -z $existed ] 
+   then 
+     image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
+     sleep 80
+     aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
+     sleep 40
+     aws autoscaling update-auto-scaling-group \
 				--auto-scaling-group-name $asgname \
 				--launch-configuration-name $LCVersion \
 				--min-size 1 \
 				--desired-capacity 1\
 				--max-size 1
-    else 
-        echo "no instance existed with this instance id:$instance_id"
-    fi
+   else 
+      echo "no instance existed with this instance id:$instance_id"
+   fi
 else
-    echo "Don't Pass empty strings"
+   echo "Don't Pass empty strings $version $instance_id $asgname"
 fi
