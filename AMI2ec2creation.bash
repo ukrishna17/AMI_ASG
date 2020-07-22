5ce66a1938ebfd410a1e7b0e543b455ab36b649b
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-22 10:29:51 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -7,8 +7,10 @@ AMIVersion="AMITest_$version"
 LCVersion="LCTest_$version"
 #image_id="ami-0ef38fe45b30d30f6"
 
+
 if [ "$version" != "" ] && ["$instance_id" != "" ] && ["$asgname" != "" ]; then
-      image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
+    if [ "aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].[InstanceId] --output text" != "" ]; then 
+        image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
 	sleep 80
 	aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
 	sleep 40
@@ -18,6 +20,9 @@ if [ "$version" != "" ] && ["$instance_id" != "" ] && ["$asgname" != "" ]; then
 				--min-size 1 \
 				--desired-capacity 1\
 				--max-size 1
+     else 
+	  echo "no instance existed with the given instance"
+     fi
 else
     echo "Don't Pass empty strings"
 fi
