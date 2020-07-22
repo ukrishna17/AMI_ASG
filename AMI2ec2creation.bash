66984b3892d19537b2e1c2a1d4d9da0223b8c1a2
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-22 10:22:19 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -1,23 +1,23 @@
 version=$1
 instance_id=$2
 asgname=$3
-keyname="Terra-Key"
-groupId="terra-sg"
 lcKeyname="asg-keypair"
 lc_Sg="sg-093e2727028157daa"
 AMIVersion="AMITest_$version"
 LCVersion="LCTest_$version"
 #image_id="ami-0ef38fe45b30d30f6"
 
-image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
-
-sleep 3m
-aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
-
-sleep 2m
-aws autoscaling update-auto-scaling-group \
+if [ "$version" != "" ] && ["$instance_id" != "" ] && ["$asgname" != "" ]; then
+      image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
+	sleep 80
+	aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
+	sleep 40
+	aws autoscaling update-auto-scaling-group \
 				--auto-scaling-group-name $asgname \
 				--launch-configuration-name $LCVersion \
 				--min-size 1 \
 				--desired-capacity 1\
 				--max-size 1
+else
+    echo "Don't Pass empty strings"
+fi
