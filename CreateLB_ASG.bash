590613db7ec256833e94b817f3ae7d6b487bf3c2
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-23 11:53:50 +0530

    Update CreateLB_ASG.bash

--- CreateLB_ASG.bash
+++ CreateLB_ASG.bash
@@ -1,5 +1,5 @@
 aws elb create-load-balancer \
 --load-balancer-name TestLB \
---listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 \
+--listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" \
 --subnets subnet-06c92527 subnet-36994238 subnet-4f3e0b71 subnet-58f54415 subnet-95e002ca subnet-9b3cdafd \
 --security-groups sg-0db5158fbc1023a95
