958b88943183904e8f9007435006587c13bd42b2
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-23 11:35:51 +0530

    Update CreateLB_ASG.bash

--- CreateLB_ASG.bash
+++ CreateLB_ASG.bash
@@ -3,4 +3,4 @@ aws elb create-load-balancer
 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 \
 --subnets subnet-06c92527,subnet-36994238,subnet-4f3e0b71,subnet-58f54415,subnet-95e002ca,subnet-9b3cdafd \
 --security-groups subnet-9b3cdafd \ 
---tags Key=Name,Value="test elb"
+--tags Key="Name",Value="test elb"
