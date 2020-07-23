f977ebc2b9cd618a14cc3154ba75041d59552ac6
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-23 10:55:44 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -13,9 +13,9 @@ LCVersion="LCTest_$version"
 #then
 #   if [ -z $existed ]; then 
      image_id=$(aws ec2 create-image --instance-id $instance_id --name $AMIVersion --description "AMI for my webserver $version" --query ImageId --output text)
- #    sleep 80
+     sleep 100
      aws autoscaling create-launch-configuration --launch-configuration-name $LCVersion --key-name $lcKeyname --image-id $image_id --user-data file://launchwebsite.sh --instance-type t2.micro --security-groups $lc_Sg 
- #    sleep 40
+     sleep 80
      aws autoscaling update-auto-scaling-group \
 				--auto-scaling-group-name $asgname \
 				--launch-configuration-name $LCVersion \
