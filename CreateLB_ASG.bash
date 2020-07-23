d03ca68676371204611b270954f51cca35405a7d
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-23 12:45:46 +0530

    Update CreateLB_ASG.bash

--- CreateLB_ASG.bash
+++ CreateLB_ASG.bash
@@ -7,10 +7,10 @@ aws elb create-load-balancer \
 sleep 40
 
 aws autoscaling create-auto-scaling-group \
-    --auto-scaling-group-name "my-asg" \ 
-    --launch-configuration-name "asg-LC" \
-    --load-balancer-names "TestLB" \
+    --auto-scaling-group-name my-asg \ 
+    --launch-configuration-name asg-LC \
+    --load-balancer-names TestLB \
     --subnets subnet-06c92527 subnet-36994238 subnet-4f3e0b71 subnet-58f54415 subnet-95e002ca subnet-9b3cdafd \
     --min-size 1 \
-	--desired-capacity 1\
-	--max-size 1
+    --desired-capacity 1\
+    --max-size 1
