cba056fde28f030acb16267467ad8a2c65c174f6
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-26 18:52:20 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -5,18 +5,19 @@ AMIVersion="AMITest_V2"
 LCVersion="LCTest_V2"
 #image_id="ami-0ef38fe45b30d30f6"
 #existed=$(aws ec2 describe-instances --instance-ids $instance_id --query Reservations[*].Instances[*].[InstanceId] --output text)
-instance_id="i-0e968c6cfafbcb7d1"
-#instance_id=$(aws ec2 run-instances \
-#        --image-id ami-0ac80df6eff0e70b5 \
-#        --region us-east-1 \
-#        --count 1 \
-#        --instance-type t2.micro \
-#        --key-name $lcKeyname \
-#        --security-group-ids "sg-0b29a2c1579c9651a" \
-#        --user-data file://launchwebsite.sh \
-#        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pilotinstance}]' \
-#        --query 'Instances[0].InstanceId')
-#sleep 2m
+#instance_id="i-0e968c6cfafbcb7d1"
+instance_id=$(aws ec2 run-instances \
+        --image-id ami-0ac80df6eff0e70b5 \
+        --region us-east-1 \
+        --count 1 \
+        --instance-type t2.micro \
+        --key-name $lcKeyname \
+        --security-group-ids "sg-0b29a2c1579c9651a" \
+        --user-data file://launchwebsite.sh \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pilotinstance}]' \
+        --query 'Instances[0].InstanceId' \
+	--output text)
+sleep 100
 #if [ -z $version -a -z $instance_id -a -z $asgname ] 
 #then
 #   if [ -z $existed ]; then 
