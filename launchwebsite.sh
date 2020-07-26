d11d82b0476ba3bb96aceb393a02f812bab9050e
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-26 20:02:40 +0530

    Update launchwebsite.sh

--- launchwebsite.sh
+++ launchwebsite.sh
@@ -4,7 +4,7 @@ sudo apt-get install git apache2 -y
 git clone https://github.com/ukrishna17/AMI_ASG.git
 sudo systemctl stop apache2
 sudo rm -rf /var/www/html/*
-sudo cp -r index.html /var/www/html/
+sudo cp -r AMI_ASG/index.html /var/www/html/
 sudo systemctl enable apache2
 sudo systemctl restart apache2
 #sudo wget https://www.tooplate.com/zip-templates/2115_marvel.zip
