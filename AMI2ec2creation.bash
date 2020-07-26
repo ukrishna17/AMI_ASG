dd3825be009fe7be8826060ad6c324892139d79c
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-26 19:30:49 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -30,7 +30,7 @@ sleep 100
 				--auto-scaling-group-name $asgname \
 				--launch-configuration-name $LCVersion \
 				--min-size 1 \
-				--desired-capacity 5\
+				--desired-capacity 4\
 				--max-size 10
    sleep 50				
    aws autoscaling update-auto-scaling-group \
