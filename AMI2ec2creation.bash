46bfd512378acff50c1b64713037caeca96d4ee3
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-26 19:29:04 +0530

    Update AMI2ec2creation.bash

--- AMI2ec2creation.bash
+++ AMI2ec2creation.bash
@@ -14,7 +14,7 @@ instance_id=$(aws ec2 run-instances \
         --key-name $lcKeyname \
         --security-group-ids "sg-0b29a2c1579c9651a" \
         --user-data file://launchwebsite.sh \
-        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,value=pilotinstance}]' \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pilotinstance}]' \
         --query 'Instances[0].InstanceId' \
 	--output text)
 sleep 100
