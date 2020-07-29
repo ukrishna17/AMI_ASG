c1d480a03208ed1250fe6e428f3e740d59c2d7e8
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-29 11:41:47 +0530

    Update EC2Creation.bash

--- EC2Creation.bash
+++ EC2Creation.bash
@@ -3,7 +3,7 @@ aws ec2 run-instances \
         --region us-east-1 \
         --count 1 \
         --instance-type t2.micro \
-        --key-name $lcKeyname \
+        --key-name "terra-sg" \
         --security-group-ids "sg-0b29a2c1579c9651a" \
         --user-data file://launchwebsite.sh \
         --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pilotinstance}]' \
