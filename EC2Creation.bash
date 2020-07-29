b2336dc1cc6e44e9641b9a9f040a8253a4f10dad
ukrishna17 <62894768+ukrishna17@users.noreply.github.com>
2020-07-29 11:40:19 +0530

    Update and rename EC2Creation to EC2Creation.bash

---
+++ EC2Creation.bash
@@ -0,0 +1,11 @@
+aws ec2 run-instances \
+        --image-id ami-0ac80df6eff0e70b5 \
+        --region us-east-1 \
+        --count 1 \
+        --instance-type t2.micro \
+        --key-name $lcKeyname \
+        --security-group-ids "sg-0b29a2c1579c9651a" \
+        --user-data file://launchwebsite.sh \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=pilotinstance}]' \
+        --query 'Instances[0].InstanceId' \
+	--output text
